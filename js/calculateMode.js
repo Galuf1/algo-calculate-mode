1716804a59cbdcacf912a8f01b695d5496b2155c
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-29 20:16:36 +0200

    js with maps

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,23 @@
-const calculateMode = () => {
+exports.calculateMode = (lista) => {
+    let freq = new Map()
+    for (i of lista) {
+        freq.set(i,0)
+    }
+    
+    for (x of lista){
+        let change = freq.get(x) + 1
+        freq.set(x,change)
+    }
+    
+    const maxi = Math.max(...freq.values())
+    
+    result = []
 
+    for (let [key,value] of freq) {
+        if (value === maxi)  {
+            result.push(key)
+        }
+    }
+    c
+    return result
 }
--- js/calculateModeSpec.js
+++ js/calculateModeSpec.js
@@ -1,1 +1,10 @@
-// Write your own unit tests here!
+var shallowEqualArrays = require('shallow-equal/arrays');
+var calc = require("./calculateMode")
+
+console.log(shallowEqualArrays(calc.calculateMode([1,2,3,3]),[3]))
+console.log(shallowEqualArrays(calc.calculateMode([4.5, 0, 0]),[0]))
+console.log(shallowEqualArrays(calc.calculateMode([1.5, -1, 1, 1.5]),[1.5]))
+console.log(shallowEqualArrays(calc.calculateMode([1,1,2,2]),[1,2]))
+console.log(shallowEqualArrays(calc.calculateMode([1,2,3]),[1,2,3]))
+console.log(shallowEqualArrays(calc.calculateMode(["who", "what", "where", "who"]),["who"]))
+
