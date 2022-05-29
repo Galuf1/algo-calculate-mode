459be09ee460c7c658a2ba1b54e1ccd632d4fc0c
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-29 19:18:46 +0200

    solver python with frequency counter

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,14 @@
-def calculate_mode():
-    pass
+def calculate_mode(lista):
+    freq = {}
+    for i in lista:
+        freq[i] = freq.get(i,0) +1
+    # sortedfreq = {x:y for x,y in sorted(freq.items(), key=lambda item:item[1],reverse=True)}
+    
+    maxfreq = max(freq,key=freq.get)
+    result = [i for i in freq if freq.get(i) == freq.get(maxfreq)]
+    
+    return result
+    
+
+
+
